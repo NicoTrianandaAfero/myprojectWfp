345c3ed586c698e96aabd4545db27b2219bd2915
NicoTrianandaAfero <nikoafero2707@gmail.com>
2022-03-01 14:40:53 +0700

    tugasWeek3

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) { //blue print adalah
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps(); //membuat 2 coloum
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users'); //database hapus tabel user
+    }
+    //ddl pembuatan struktur hingga menghapus struktur(drop)
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('MyStore');
+});
+
+
+Route::get('wfp/{kelas}',function($kelas) { 
+    if($kelas == "C" || $kelas == "c")
+        return 'WFP kelas saya ' .$kelas;
+    
+    else {
+        return 'Bukan Kelas Saya';
+    }
+});
+         
+ 
+//Route::view('/welcome', 'welcome');
+
+Route::get('Nico', function(){
+    return "Hello WFP C";
+});
+
+Route::get('greating', function(){
+    return view ('welcome',['name'=>'Nico']);
+});
